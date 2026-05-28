18683002684c0a44fa3193d16d805e8ca31b6529
brettmt2 <btpeach.2@gmail.com>
2026-05-27 22:04:02 -0400

    line detector node visitor class

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,24 @@
 from dataclasses import dataclass
 from typing import Callable
 import functools
+import ast
+
+class LineDetectionNodeVisitor(ast.NodeVisitor):
+    def __init__(self, start, end):
+        super().__init__()
+        self.start: int = start
+        self.end: int = end
+        self.start_stmt_expr: ast.stmt | ast.expr = None
+        self.end_stmt_expr: ast.stmt | ast.expr = None
+
+    def generic_visit(self, node):
+        if isinstance(node, ast.expr) or isinstance(node, ast.stmt):
+            if node.lineno == self.start:
+                self.start_stmt_expr = node
+            elif node.lineno == self.end:
+                self.end_stmt_expr = node
+        return super().generic_visit(node)
+
 
 @dataclass
 class Handler:
