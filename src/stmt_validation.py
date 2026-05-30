3f88f941353ad6e4a012937d46466e28178f6343
brettmt2 <btpeach.2@gmail.com>
2026-05-30 19:56:18 -0400

    file separation

--- src/stmt_validation.py
+++ src/stmt_validation.py
@@ -1,9 +1,8 @@
-from dataclasses import dataclass
-from typing import Callable
-import functools
 import ast
-from errors import LambentLineDetectionError
 import inspect
+from typing import Callable
+
+from errors import LambentLineDetectionError
 
 class LineValidatorNodeVisitor(ast.NodeVisitor):
     def __init__(self, start, end, func):
@@ -54,17 +53,3 @@ class LineValidatorNodeVisitor(ast.NodeVisitor):
         self.visit(self.tree)
         self._stmt_function_index_validation()
         self._stmt_indentation_validation()
-
-@dataclass
-class Handler:
-    on_error: Callable
-    exceptions: tuple = (Exception, )
-
-    def __call__(self, func: Callable):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except self.exceptions as e:
-                return self.on_error(e)
-        return wrapper
