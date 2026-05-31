1c4d00a4e286a7c5762a46de363cbc9c83bb5602
brettmt2 <btpeach.2@gmail.com>
2026-05-30 20:19:53 -0400

    parent block validation for statements

--- src/stmt_validation.py
+++ src/stmt_validation.py
@@ -23,9 +23,18 @@ class LineValidatorNodeVisitor(ast.NodeVisitor):
         self.tree = ast.parse(source)
         self._validate()
 
+        
+    def _print_debug(self):
+        print(f"start_stmt: {ast.unparse(self.start_stmt)}")
+        print(f"end_stmt: {ast.unparse(self.end_stmt)}")
+        print(f"function_def_node: {ast.unparse(self.function_def_node)}")
+
     def generic_visit(self, node: ast.AST):
         if self.function_def_node is None and isinstance(node, ast.FunctionDef):
             self.function_def_node = node
+
+        for child in ast.iter_child_nodes(node):
+            child._parent = node
 
         if isinstance(node, ast.stmt):
             if node.lineno == self.start:
@@ -47,16 +56,25 @@ class LineValidatorNodeVisitor(ast.NodeVisitor):
         e_col = self.end_stmt.col_offset
 
         if s_col != e_col:
+            self._print_debug()
             raise LambentLineDetectionError(f"Statements are not on the same indentation level: {s_col}, {e_col}.")
         
     def _stmt_block_validation(self):
         block_stmts = (ast.If, ast.While, ast.For, ast.Try, ast.With, ast.FunctionDef, ast.ClassDef)
         # end statement must be a non block statement
         if isinstance(self.end_stmt, block_stmts):
+            self._print_debug()
             raise LambentLineDetectionError(f"End statement cannot be a block statement: (ast.If, ast.While, ast.For, ast.Try, ast.With, ast.FunctionDef, ast.ClassDef).")
+
+    def _stmt_parent_block_validation(self):
+        # two statements must be in the same block
+        if self.start_stmt._parent != self.end_stmt._parent:
+            self._print_debug()
+            raise LambentLineDetectionError("Statements must have the same parent.")
 
     def _validate(self):
         self.visit(self.tree)
         self._stmt_function_index_validation() # assumes start < end and start < 2
         self._stmt_indentation_validation() # assumes start and end stmts exist
         self._stmt_block_validation() # assumes start and end stmts have same col offset
+        self._stmt_parent_block_validation()  # assumes end_stmt is not a block statement
